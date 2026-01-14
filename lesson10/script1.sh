91e82bb3c2c0673e346fbb041d7d7fe5df400272
Irina Alekseevskaia <alekseevskaia98@gmail.com>
2026-01-14 20:30:19 +0300

    Add two scripts

--- lesson10/script1.sh
+++ lesson10/script1.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
-echo `pwd`
-echo `mkdir myfolder`
-echo `cd myfolder`
-echo ``
+
+MYFOLDER="$HOME/myfolder"
+
+mkdir -p "$MYFOLDER"
+
+{
+    echo "Привет! Это файл 1."
+    date +"%Y-%m-%d %H:%M:%S"
+} > "$MYFOLDER/file1.txt"
+
+touch "$MYFOLDER/file2.txt"
+chmod 777 "$MYFOLDER/file2.txt"
+
+< /dev/urandom tr -dc 'A-Za-z0-9' | head -c20 > "$MYFOLDER/file3.txt"
+
+touch "$MYFOLDER/file4.txt" "$MYFOLDER/file5.txt"
+
+echo "script1.sh: папка и файлы созданы."
